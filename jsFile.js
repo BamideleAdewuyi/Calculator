1c4648a77490a738bc9f743c920dbfd4ce47d1d5
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-27 18:53:22 +0000

    jsFile: Wrote function to make screen 0 on clicking CE button

--- jsFile.js
+++ jsFile.js
@@ -4,7 +4,7 @@ let operator;
 
 const nums = document.querySelector(".number");
 const clear = document.querySelector("#clear");
-
+const screen = document.querySelector("#screen");
 clear.addEventListener("click", () => {
-    
+    screen.innerText = 0;
 })
