d26315df3208f6adbabaa6ec7c92f44831b02df9
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-27 20:50:06 +0000

    jsFile.js: Made firstNum and secondNum variables strings

--- jsFile.js
+++ jsFile.js
@@ -1,10 +1,11 @@
-let firstNum = 0;
-let secondNum = 0;
+let firstNum = "0";
+let secondNum = "0";
 let operator;
 
 const nums = document.querySelectorAll(".number");
 const clear = document.querySelector("#clear");
 const screen = document.querySelector("#screen");
+
 clear.addEventListener("click", () => {
-    screen.innerText = 0;
+    screen.innerText = "0";
 })
